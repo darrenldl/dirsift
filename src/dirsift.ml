2138d1d21e51bba058706cef96947a1bec3f3ef9
Darren Ldl <darrenldldev@gmail.com>
2021-02-25 21:47:33 +1100

    Formatting

--- src/dirsift.ml
+++ src/dirsift.ml
@@ -11,28 +11,19 @@ let rec dir_matches_typ dir typ =
   match typ with
   | Git ->
     let sub_dirs =
-      try
-        Sys.readdir dir
-      with
-      | _ -> failwith "Failed to read directory"
+      try Sys.readdir dir with _ -> failwith "Failed to read directory"
     in
     Array.mem ".git" sub_dirs
-  | Hidden ->
-    String.get dir 0 = '.'
-  | Not x ->
-    not (dir_matches_typ dir x)
+  | Hidden -> dir.[0] = '.'
+  | Not x -> not (dir_matches_typ dir x)
 
 let run (typs : dir_typ list) (dir : string) =
   let sub_dirs =
-    try
-      Sys.readdir dir
-      |> Array.to_list
-    with
-    | _ -> failwith "Failed to read directory"
+    try Sys.readdir dir |> Array.to_list
+    with _ -> failwith "Failed to read directory"
   in
   sub_dirs
-  |> List.filter (fun dir ->
-      List.for_all (dir_matches_typ dir) typs)
+  |> List.filter (fun dir -> List.for_all (dir_matches_typ dir) typs)
   |> List.iter print_endline
 
 let typ_arg =
@@ -44,16 +35,15 @@ let typ_arg =
       ("not-hidden", Not Hidden);
     ]
   in
-  let doc =
-    "$(docv) is one of git, not-git"
-  in
-  Arg.(value & opt_all (enum typs) [] & info ["t"; "type"] ~doc ~docv:"TYPE")
+  let doc = "$(docv) is one of git, not-git" in
+  Arg.(value & opt_all (enum typs) [] & info [ "t"; "type" ] ~doc ~docv:"TYPE")
 
-let dir_arg =
-  Arg.(value & pos 0 dir "." & info [])
+let dir_arg = Arg.(value & pos 0 dir "." & info [])
 
 let cmd =
-  let doc = "Filter directories which satisfy all directory types constraints" in
+  let doc =
+    "Filter directories which satisfy all directory types constraints"
+  in
   (Term.(const run $ typ_arg $ dir_arg), Term.info "dirsift" ~doc)
 
 let () = Term.(exit @@ Term.eval cmd)
